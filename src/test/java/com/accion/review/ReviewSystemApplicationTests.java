7977895705e0390aec360b8a9d859e1140629b9d
SheikhAsiyaSultana <sheikhasiyasultana@gmail.com>
2017-11-17 15:24:04 +0530

    FIRST COMMIT

---
+++ src/test/java/com/accion/review/ReviewSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.accion.review;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReviewSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
